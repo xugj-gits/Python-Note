af0319ffaacc932b4ecbd897a105711730cefa35
高效码农 <615792701@qq.com>
2019-02-26 18:46:54 +0800

    四、面向对象编程

--- 四、面向对象编程/student.py
+++ 四、面向对象编程/student.py
@@ -1,9 +1,16 @@
 # -*_ coding: UTF-8 -*-
 
 class Student:
-    def __init__(self, name, score):
+    def __init__(self, name, score, gender):
         self.name = name
         self.score = score
+        self.__gender = gender
+
+    def get_gender(self):
+        return self.__gender
+
+    def set_gender(self, gender):
+        self.__gender = gender
 
     def print_score(self):
         print('%s: %s' % (self.name, self.score))
@@ -16,8 +23,8 @@ class Student:
         else:
             return 'C'
 
-bart = Student('Bart Simpson', 59)
-lisa = Student('Lisa Simpson', 87)
+bart = Student('Bart Simpson', 59, '男')
+lisa = Student('Lisa Simpson', 87, '女')
 
 print('bart.name=', bart.name)
 print('bart.score=', bart.score)
@@ -26,3 +33,13 @@ bart.print_score()
 
 print('grade of Bart:', bart.get_grade())
 print('grade of Lisa:', lisa.get_grade())
+
+# 不建议这样使用
+bart._Student__gender
+
+# 会报错：'Student' object has no attribute '__gender'
+# bart.__gender
+
+
+
+print('gender of Bart:', bart.get_gender())
